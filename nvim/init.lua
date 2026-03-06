a29b16b3074a6be9343cd80dff32f9c58d116699
cya <cya@CYA-2.local>
2026-03-06 23:40:46 +0800

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -185,5 +185,224 @@ require("lazy").setup({
 				require("rainbow-delimiters").enable(vim.api.nvim_get_current_buf())
 			end,
 		},
+		{
+			"folke/snacks.nvim",
+			opts = {
+				indent = {
+					enabled = true,
+					chunk = {
+						enabled = true,
+						char = {
+							corner_top = "╭",
+							corner_bottom = "╰",
+							horizontal = "─",
+							vertical = "│",
+							arrow = ">",
+						},
+					},
+				},
+			},
+		},
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			lazy = false,
+			config = function()
+				require("catppuccin").setup({
+					flavour = "auto",
+					background = {
+						light = "latte",
+						dark = "mocha",
+					},
+					transparent_background = false,
+					float = {
+						transparent = false,
+						solid = false,
+					},
+					show_end_of_buffer = false,
+					term_colors = false,
+					dim_inactive = {
+						enabled = false,
+						shade = "dark",
+						percentage = 0.15,
+					},
+					no_italic = false,
+					no_bold = false,
+					no_underline = false,
+					styles = {
+						comments = { "italic" },
+						conditionals = { "italic" },
+						loops = {},
+						functions = {},
+						keywords = {},
+						strings = {},
+						variables = {},
+						numbers = {},
+						booleans = {},
+						properties = {},
+						types = {},
+						operators = {},
+						miscs = {},
+					},
+					lsp_styles = {
+						virtual_text = {
+							errors = { "italic" },
+							hints = { "italic" },
+							warnings = { "italic" },
+							information = { "italic" },
+							ok = { "italic" },
+						},
+						underlines = {
+							errors = { "underline" },
+							hints = { "underline" },
+							warnings = { "underline" },
+							information = { "underline" },
+							ok = { "underline" },
+						},
+						inlay_hints = {
+							background = true,
+						},
+					},
+					color_overrides = {
+						mocha = {
+							base = "#11111b",
+							mantle = "#11111b",
+						},
+					},
+					custom_highlights = function(colors)
+						return {
+							Comment = { fg = colors.subtext0 },
+							CursorLineNr = { fg = colors.green, style = { "bold" } },
+						}
+					end,
+					highlight_overrides = {
+						all = function(colors)
+							return {}
+						end,
+					},
+					default_integrations = true,
+					auto_integrations = true,
+					integrations = {
+						aerial = false,
+						alpha = false,
+						barbar = false,
+						barbecue = {
+							dim_dirname = false,
+							bold_basename = false,
+							dim_context = false,
+							alt_background = false,
+						},
+						beacon = false,
+						blink_cmp = {
+							style = "bordered",
+						},
+						blink_indent = false,
+						blink_pairs = false,
+						buffon = false,
+						coc_nvim = false,
+						colorful_winsep = {
+							enabled = false,
+							color = "red",
+						},
+						dashboard = false,
+						diffview = true,
+						dropbar = {
+							enabled = false,
+							color_mode = false,
+						},
+						fern = false,
+						fidget = false,
+						flash = true,
+						fzf = false,
+						gitgraph = false,
+						gitsigns = true,
+						grug_far = false,
+						harpoon = false,
+						headlines = false,
+						hop = false,
+						indent_blankline = {
+							enabled = false,
+							scope_color = "lavender",
+							colored_indent_levels = true,
+						},
+						leap = false,
+						lightspeed = false,
+						lir = {
+							enabled = false,
+							git_status = false,
+						},
+						lsp_saga = false,
+						lualine = {
+							all = function(colors)
+								return {
+									normal = {
+										a = { bg = colors.lavender, gui = "italic" },
+										b = { fg = colors.lavender },
+									},
+								}
+							end,
+							macchiato = {
+								normal = {
+									a = { bg = "#abcdef" },
+								},
+							},
+						},
+						markview = false,
+						mason = true,
+						mini = {
+							enabled = false,
+							indentscope_color = "lavender",
+						},
+						neotree = false,
+						neogit = false,
+						neotest = false,
+						noice = false,
+						notifier = false,
+						cmp = false,
+						copilot_vim = false,
+						dap = true,
+						dap_ui = true,
+						navic = {
+							enabled = true,
+							custom_bg = "NONE",
+						},
+						notify = false,
+						nvim_surround = false,
+						nvimtree = false,
+						treesitter_context = true,
+						ts_rainbow2 = false,
+						ts_rainbow = false,
+						ufo = false,
+						window_picker = false,
+						octo = false,
+						overseer = true,
+						pounce = false,
+						rainbow_delimieters = true,
+						snacks = {
+							enabled = true,
+							indent_scope_color = "lavender",
+						},
+						symbols_outline = false,
+						telekasten = false,
+						telescope = {
+							enabled = false,
+						},
+						lsp_trouble = true,
+						dadbod_ui = false,
+						gitgutter = false,
+						illuminate = {
+							enabled = true,
+							lsp = true,
+						},
+						sandwich = false,
+						signify = false,
+						vim_sneak = false,
+						vimwiki = false,
+						which_key = true,
+					},
+				})
+				vim.cmd.colorscheme("catppuccin")
+			end,
+		},
 	},
 })
